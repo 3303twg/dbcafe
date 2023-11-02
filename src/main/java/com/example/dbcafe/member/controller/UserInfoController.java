21ecadc3c95c0d76d71bb68f52aad2e7f77373dd
DESKTOP-J3D61FN\sorp0 <sorp0226@gmail.com>
2023-11-02 20:55:33 +0900

    계정정보 수정기능 완료 입력폼 내용 수정 필요

--- src/main/java/com/example/dbcafe/member/controller/UserInfoController.java
+++ src/main/java/com/example/dbcafe/member/controller/UserInfoController.java
@@ -36,11 +36,44 @@ public class UserInfoController {
     private final UserRepository userRepository;
 
 
-    @GetMapping("/mypage")
-    public String mypage(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model){
-        User user = principalDetails.getUser();
-        model.addAttribute("userInfo",user);
-        return "mypage";}
+    // 유저 페이지 접속
+    @GetMapping("/mypage/{userId}")
+    public String userPage(@PathVariable("userId") Long id, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        // 로그인이 되어있는 유저의 id와 유저 페이지에 접속하는 id가 같아야 함
+        if (principalDetails.getUser().getId() == id) {
+
+            model.addAttribute("userInfo", userInfoService.findUser(id));
+
+            return "/MyPage";
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    // 회원 정보 수정
+    @GetMapping("/mypage/update/{userId}")
+    public String userModify(@PathVariable("userId") Long id, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        // 로그인이 되어있는 유저의 id와 수정페이지에 접속하는 id가 같아야 함
+        if (principalDetails.getUser().getId() == id) {
+            model.addAttribute("userInfo", userInfoService.findUser(id));
+            return "information";
+        } else {
+            return "redirect:/";
+        }
+
+    }
+
+    // 수정 실행
+    @PostMapping("/mypage/update/{userId}")
+    public String userUpdate(@PathVariable("userId") Long id, User user, Model model) {
+
+        userInfoService.userModify(user);
+        model.addAttribute("message", "계정정보 수정이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/");
+        return "message";
+    }
+
+
 
 //    @GetMapping("/mypage/qna")
 //    public String myQnA(){
@@ -57,20 +90,6 @@ public class UserInfoController {
         List<OrderEntity> userOrders = orderService.getOrdersForUser(userId); // 해당 유저의 주문 데이터를 가져오는 메서드를 구현해야 합니다.
         model.addAttribute("orders", userOrders);
         return"MyOrder";
-    }
-
-    @GetMapping("/mypage/update")
-    public String infoModify(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model){
-        User user = principalDetails.getUser();
-        model.addAttribute("userInfo",user);
-        return "information";
-    }
-    @PostMapping("/mypage/update")
-    public String infoUpdate(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model){
-        User usertemp = userInfoService.getUserByUsername(principalDetails.getUser().getUsername());
-
-
-        return "massage";
     }
 
 
